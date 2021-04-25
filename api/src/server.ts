895e7d097459457a5b4eb3e14290e72b3b989ca7
polkom21 <polkom21@gmail.com>
2021-04-25 20:20:16 +0200

    basic api with websocket

---
+++ api/src/server.ts
@@ -0,0 +1,36 @@
+import * as express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
+
+const app = express();
+
+//initialize a simple http server
+const server = http.createServer(app);
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+
+    //connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+
+        //log the received message and send it back to the client
+        console.log('received: %s', message);
+        ws.send(`Hello, you sent -> ${message}`);
+    });
+
+    //send immediatly a feedback to the incoming connection
+    ws.send('Hi there, I am a WebSocket server');
+});
+
+app.get('/:ws', (req, res) => {
+    console.log(req.params.ws)
+    res.send('Look on TV')
+})
+
+//start our server
+server.listen(process.env.PORT || 8999, () => {
+    // @ts-ignore
+    console.log(`Server started on port ${server.address().port}`);
+});
